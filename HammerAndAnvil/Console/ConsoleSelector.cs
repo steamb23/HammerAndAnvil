7e3a9a91ad2472a960f15cf5dd69be772cdb9882
steamb23 <steamb23@gmail.com>
2016-01-03 01:09:42 +0900

    char 형대신 ConsoleKey형을 사용하도록 수정. char 형은 호환용으로 냅둠.

--- HammerAndAnvil/Console/ConsoleSelector.cs
+++ HammerAndAnvil/Console/ConsoleSelector.cs
@@ -12,12 +12,12 @@ namespace System
         {
             public struct SelectElement
             {
-                public SelectElement(char key, Action action)
+                public SelectElement(ConsoleKey key, Action action)
                 {
                     this.key = key;
                     this.action = action;
                 }
-                public char key;
+                public ConsoleKey key;
                 public Action action;
                 public void Action()
                 {
@@ -28,30 +28,30 @@ namespace System
                 }
             }
             static List<SelectElement> selectList = new List<SelectElement>();
-            public static void Add(char key, string text, Action action)
+            static bool isHaveEnterKey;
+            public static void Add(ConsoleKey key, string text, Action action)
             {
-                key = char.ToUpper(key);
-
                 Console.Write(" ");
                 Console.ForegroundColor = ConsoleColor.Yellow;
-                Console.Write(key);
+                Console.Write(ConsoleKeyToString(key));
                 Console.ResetColor();
                 Console.WriteLine("){0}", text);
                 selectList.Add(new SelectElement(key, action));
+                if (key == ConsoleKey.Enter)
+                    isHaveEnterKey = true;
             }
-            public static void Read(char defaultKey)
+            public static void Add(char key, string text, Action action)
             {
-                defaultKey = char.ToUpper(defaultKey);
+                Add(charToConsoleKey_Table[key], text, action);
+            }
+            public static void Read(ConsoleKey defaultKey)
+            {
                 bool pass = false;
                 SelectElement selectedElement = new SelectElement();
-
                 while (!pass)
                 {
-                    char key = char.ToUpper(Console.ReadKey(true).KeyChar);
-                    char kokey = '\0';
-                    if (ko_enTable.TryGetValue(key, out kokey))
-                        key = kokey;
-                    key = (key != '\r') ? key : defaultKey;
+                    ConsoleKey key = Console.ReadKey(true).Key;
+                    key = (!isHaveEnterKey && key != ConsoleKey.Enter) ? key : defaultKey;
                     foreach (var selectElement in selectList)
                     {
                         if (key == selectElement.key)
@@ -65,9 +65,15 @@ namespace System
                 Clear();
                 selectedElement.Action();
             }
+
+            internal static void Read(char defaultKey)
+            {
+                Read(charToConsoleKey_Table[defaultKey]);
+            }
+
             public static void Read()
             {
-                Read('\0');
+                Read(ConsoleKey.Enter);
             }
             public static void Clear()
             {
@@ -77,41 +83,108 @@ namespace System
                     ClearLine();
                 }
                 selectList.Clear();
+                isHaveEnterKey = false;
             }
-            //For Korean
-            static Dictionary<char, char> ko_enTable = new Dictionary<char, char>
+            public static string ConsoleKeyToString(ConsoleKey key)
             {
-                { 'ㅃ', 'Q'},
-                { 'ㅉ', 'W'},
-                { 'ㄸ', 'E'},
-                { 'ㄲ', 'R'},
-                { 'ㅆ', 'T'},
-                { 'ㅂ', 'Q'},
-                { 'ㅈ', 'W'},
-                { 'ㄷ', 'E'},
-                { 'ㄱ', 'R'},
-                { 'ㅅ', 'T'},
-                { 'ㅛ', 'Y'},
-                { 'ㅕ', 'U'},
-                { 'ㅑ', 'I'},
-                { 'ㅐ', 'O'},
-                { 'ㅔ', 'P'},
-                { 'ㅁ', 'A'},
-                { 'ㄴ', 'S'},
-                { 'ㅇ', 'D'},
-                { 'ㄹ', 'F'},
-                { 'ㅎ', 'G'},
-                { 'ㅗ', 'H'},
-                { 'ㅓ', 'J'},
-                { 'ㅏ', 'K'},
-                { 'ㅣ', 'L'},
-                { 'ㅋ', 'Z'},
-                { 'ㅌ', 'X'},
-                { 'ㅊ', 'C'},
-                { 'ㅍ', 'V'},
-                { 'ㅠ', 'B'},
-                { 'ㅜ', 'N'},
-                { 'ㅡ', 'M'}
+                switch (key)
+                {
+                    case ConsoleKey.UpArrow:
+                        return "↑";
+                    case ConsoleKey.DownArrow:
+                        return "↓";
+                    case ConsoleKey.RightArrow:
+                        return "→";
+                    case ConsoleKey.LeftArrow:
+                        return "←";
+                    case ConsoleKey.D1:
+                        return "1";
+                    case ConsoleKey.D2:
+                        return "2";
+                    case ConsoleKey.D3:
+                        return "3";
+                    case ConsoleKey.D4:
+                        return "4";
+                    case ConsoleKey.D5:
+                        return "5";
+                    case ConsoleKey.D6:
+                        return "6";
+                    case ConsoleKey.D7:
+                        return "7";
+                    case ConsoleKey.D8:
+                        return "8";
+                    case ConsoleKey.D9:
+                        return "9";
+                    case ConsoleKey.D0:
+                        return "0";
+                    default:
+                        return key.ToString();
+                }
+            }
+            public static Dictionary<char, ConsoleKey> charToConsoleKey_Table = new Dictionary<char, ConsoleKey>
+            {
+                {'1',ConsoleKey.D1 },
+                {'2',ConsoleKey.D2 },
+                {'3',ConsoleKey.D3 },
+                {'4',ConsoleKey.D4 },
+                {'5',ConsoleKey.D5 },
+                {'6',ConsoleKey.D6 },
+                {'7',ConsoleKey.D7 },
+                {'8',ConsoleKey.D8 },
+                {'9',ConsoleKey.D9 },
+                {'0',ConsoleKey.D0 },
+                {'q',ConsoleKey.Q },
+                {'w',ConsoleKey.W },
+                {'e',ConsoleKey.E },
+                {'r',ConsoleKey.R },
+                {'t',ConsoleKey.T },
+                {'y',ConsoleKey.Y },
+                {'u',ConsoleKey.U },
+                {'i',ConsoleKey.I },
+                {'o',ConsoleKey.O },
+                {'p',ConsoleKey.P },
+                {'a',ConsoleKey.A },
+                {'s',ConsoleKey.S },
+                {'d',ConsoleKey.D },
+                {'f',ConsoleKey.F },
+                {'g',ConsoleKey.G },
+                {'h',ConsoleKey.H },
+                {'j',ConsoleKey.J },
+                {'k',ConsoleKey.K },
+                {'l',ConsoleKey.L },
+                {'z',ConsoleKey.Z },
+                {'x',ConsoleKey.X },
+                {'c',ConsoleKey.C },
+                {'v',ConsoleKey.V },
+                {'b',ConsoleKey.B },
+                {'n',ConsoleKey.N },
+                {'m',ConsoleKey.M },
+                {'Q',ConsoleKey.Q },
+                {'W',ConsoleKey.W },
+                {'E',ConsoleKey.E },
+                {'R',ConsoleKey.R },
+                {'T',ConsoleKey.T },
+                {'Y',ConsoleKey.Y },
+                {'U',ConsoleKey.U },
+                {'I',ConsoleKey.I },
+                {'O',ConsoleKey.O },
+                {'P',ConsoleKey.P },
+                {'A',ConsoleKey.A },
+                {'S',ConsoleKey.S },
+                {'D',ConsoleKey.D },
+                {'F',ConsoleKey.F },
+                {'G',ConsoleKey.G },
+                {'H',ConsoleKey.H },
+                {'J',ConsoleKey.J },
+                {'K',ConsoleKey.K },
+                {'L',ConsoleKey.L },
+                {'Z',ConsoleKey.Z },
+                {'X',ConsoleKey.X },
+                {'C',ConsoleKey.C },
+                {'V',ConsoleKey.V },
+                {'B',ConsoleKey.B },
+                {'N',ConsoleKey.N },
+                {'M',ConsoleKey.M }
             };
         }
     }
